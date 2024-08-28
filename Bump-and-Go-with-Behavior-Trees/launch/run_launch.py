0e7dfdb043da0b4b1676529d431b34f7e1a46b4e
SaiVinay023 <saivinay023@gmail.com>
2024-08-28 06:23:53 +0200

    Bump and go with behaviour trees

---
+++ Bump-and-Go-with-Behavior-Trees/launch/run_launch.py
@@ -0,0 +1,25 @@
+# Import necessary modules from launch
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='bt_bumpgo',
+            executable='bt_bumpgo_main',
+            name='bt_bumpgo_main',
+            output='screen',
+            parameters=[{'use_sim_time': True}],
+            remappings=[
+                ('input_scan', '/scan_raw'),
+                ('output_vel', '/cmd_vel'),
+            ]
+        ),
+        Node(
+            package='monitor',
+            executable='monitor_0',
+            name='monitor_0',
+            output='screen',
+            parameters=[{'use_sim_time': True}],
+        ),
+    ])
